b0d8977b7c74130954d04b5250d6a3dee8569b4e
EswarR99 <53962339+EswarR99@users.noreply.github.com>
2020-02-01 12:21:27 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -6,7 +6,7 @@ public class Hello{
 
                          System.out.println("Hello World ..."+i);
                          char;
-
+                         int j=a;
                          }
 
          }
